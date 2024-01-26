570905c9eb72213477c255f61b8112672cd26d9b
Diego <101148313+Dalistor@users.noreply.github.com>
2024-01-26 16:34:55 -0300

    first commit

---
+++ src/main/java/com/tasks/tasks/Tasks.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tasks.tasks;
+
+/**
+ *
+ * @author Dalistor
+ */
+public class Tasks {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
